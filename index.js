7064cf24441ee2699d6f6da064781fd2f2c0a399
PrincetonJRose <decoyzpr@gmail.com>
2019-04-22 17:29:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function addingEventListener("input#input") {
+function addingEventListener() {
   const input = document.getElementById('input');
   input.addEventListener('click', function(event) {
     alert('I was clicked!');
